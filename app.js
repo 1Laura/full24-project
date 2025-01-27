6a8d65d894ee7f62bee5033cde084a487d7e5eb0
Laura <baltrukone@gmail.com>
2025-01-27 09:55:07 +0200

    get random cat img with promise

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const getRandomEmoji = require('./modules/emojiList');
 const getRandomName = require('./modules/nameList');
-const getRandomAge = require('./modules/randomNumber')
+const getRandomAge = require('./modules/randomNumber');
+const animalName = require("random-animal-name");
+const randomCatImage = require("random-cat-img")
 
 function createUser() {
     const user = {
@@ -18,3 +20,19 @@ function doStuff() {
 
 doStuff();
 
+
+function getAnimalName() {
+    const name = animalName();
+    console.log(name)
+}
+
+getAnimalName();
+
+function getRandomCatImg() {
+    randomCatImage().then(catImg => {
+        console.log(catImg.message);
+    });
+
+}
+
+getRandomCatImg()
