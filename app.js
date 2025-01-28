a40c4591e841b494fda24e2705241038b3f8812e
Laura <baltrukone@gmail.com>
2025-01-28 08:06:28 +0200

    need to do task

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
-const getRandomEmoji = require('./modules/emojiList');
-const getRandomName = require('./modules/nameList');
-const getRandomAge = require('./modules/randomNumber');
 const animalName = require("random-animal-name");
 const randomCatImage = require("random-cat-img")
-
-function createUser() {
-    const user = {
-        emoji: getRandomEmoji(),
-        age: getRandomAge(100),
-        name: getRandomName()
-    };
-    return user;
-}
-
-function doStuff() {
-    const user = createUser();
-    console.log(user);
-}
-
-doStuff();
-
 
 function getAnimalName() {
     const name = animalName();
@@ -32,7 +12,6 @@ function getRandomCatImg() {
     randomCatImage().then(catImg => {
         console.log(catImg.message);
     });
-
 }
 
 getRandomCatImg()
