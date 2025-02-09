580039c42dc01d69bf10de1a22248bc5add02b48
Laura <baltrukone@gmail.com>
2025-02-09 20:05:58 +0200

    get faker data 10 items

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const express = require("express");
 // Jei pradedi naują projektą su modernia ES6 sintakse ir gali pakeisti package.json, naudok import. Jei dirbi su senesniu kodu, greičiausiai vis dar reikės require.
 // biblioteka yra kazkieno kito sukurtas modulis  https://www.npmjs.com/
 const getFakerData = require("@faker-js/faker");
 
 
-function getFaker() {
+function getFaker(number) {
 
-    console.log(getFakerData.faker.person.firstName());
+    for (let i = 0; i < number; i++) {
+        const item = {
+            firstName: getFakerData.faker.person.firstName(),
+            lastName: getFakerData.faker.person.lastName(),
+            jobTitle: getFakerData.faker.person.jobTitle(),
+            accountNumber: getFakerData.faker.finance.accountNumber(),
+            avatarImage: getFakerData.faker.image.avatar(),//url()
+            vehicleModel: getFakerData.faker.vehicle.model(),
+            locationCity: getFakerData.faker.location.city(),
+            currentDate: getFakerData.faker.date.month(),
+        }
+        console.log(item)
+    }
 }
 
-getFaker()
+getFaker(10)
 
