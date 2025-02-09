7007331043f07a5d7dce318e9a1a28e41003b31f
Laura <baltrukone@gmail.com>
2025-02-09 17:07:13 +0200

    random animal name from node library

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
+// biblioteka yra kazkieno kito sukurtas modulis  https://www.npmjs.com/
+const animalName = require("random-animal-name");
 const getEmoji = require("./modules/emojiList");
 
-app.get("/info/:text", (req, res) => {
-    console.log(req.params.text)
 
-    res.send({"value of param is": req.params.text})
-    // res.send({message:"all good"})
-})
+function runRandomAnimalName() {
+    const randomName = animalName();
+    console.log(randomName);
+}
 
-app.listen(2001)
-console.log("server run")
+runRandomAnimalName();
+
+
+// app.get("/info/:text", (req, res) => {
+//     console.log(req.params.text)
+//
+//     res.send({"value of param is": req.params.text})
+//     // res.send({message:"all good"})
+// })
+//
+// app.listen(2001)
+// console.log("server run")
