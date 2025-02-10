4cff5f6f2f67df86d166dc52695d5bc0d950f065
Laura <baltrukone@gmail.com>
2025-02-10 14:19:43 +0200

    task with user register, login done

--- app.js
+++ app.js
@@ -5,19 +5,43 @@ const {faker, fa} = require("@faker-js/faker");
 let users = [];
 
 //routes
-app.get("/create/:name", (req, res) => {
+// create backend logic:
+// 1) user should be able to register. user provides username and password;
+// 2) on registration respond success message or error message;
+// 3) if user with same name exists, do not let register;
+//
+// When user registers, let him login/
+// user should provide username and password, if username and password correct, return whole user object as response, if not correct, write error message, what is wrong (for example: bad username)
+
+app.get("/register/:name/:password", (req, res) => {
     const user = {
-        name: req.params.name,
+        username: req.params.name,
+        password: req.params.password,
         id: faker.string.uuid(),
-        lastName: faker.person.lastName(),
-        email: faker.internet.email(),
-        ipAddress: faker.internet.ip(),
-    };
+    }
+    if (user.username === null && user.password === null) {
+        return res.send({message: "Username or password not entered"});
+    }
+    const userExists = users.find(user => user.username === req.params.name)
+    // console.log(userExists);
+    if (userExists) {
+        return res.send({message: `A user with ${userExists.username} already exists.`});
+    }
     users.push(user)
-    console.log(users)
-    res.send({message: `user created ${user.id}, ${user.name} ${user.lastName}, ${user.email}, ${user.ipAddress}`});
+    res.send({message: `user ${user.username} registered`});
 })
 
+app.get("/login/:name/:password", (req, res) => {
+    const loginUser = {
+        username: req.params.name,
+        password: req.params.password,
+    }
+    const userRegistered = users.find(user => user.username === loginUser.username && user.password === loginUser.password)
+    if (!userRegistered) {
+        return res.send({message: `Such a user is not registered.`});
+    }
+    res.send({message: `user ${loginUser.username} and pass ${loginUser.password} logged in`});
+})
 app.get("/usersList", (req, res) => {
 
     res.send({users});//duomenys siunciami objekte
@@ -25,32 +49,12 @@ app.get("/usersList", (req, res) => {
 app.get("/remove/:id", (req, res) => {
     const id = req.params.id;
     const userExists = users.find(user => user.id === id);
-    if(!userExists){
-       return   res.send({message: "User doesn't exist"});
+    if (!userExists) {
+        return res.send({message: "User doesn't exist"});
     }
     users = users.filter(user => user.id !== id)
     res.send({message: "User deleted"});
 
-});
-
-
-// kaip pasiimti name -> req.params.name
-app.get("/info/:name", (req, res) => {
-    const username = req.params.name
-    console.log(req.params.name);
-    // res.send({success: true,});
-    res.send({message: "value of param is " + username,});
-});
-
-app.get("/data", (req, res) => {
-    console.log("sfsfd");
-    res.send({message: "fghdfh"});
-});
-
-app.get("/some", (req, res) => {
-    console.log("sdfsaf");
-    // res.send({success: true,});
-    res.send({message: "all good",});
 });
 
 app.listen(2002);
