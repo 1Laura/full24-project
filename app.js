e4a9c0ef453e320e81f8833cb686a612c1cbcf4a
Laura <baltrukone@gmail.com>
2025-02-10 10:59:41 +0200

    1) receives username as param, using faker gets user lastName email and IP and saves all object it to array of users (in global scope). 2) responds with array of users

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const app = express();
+const {faker, fa} = require("@faker-js/faker");
 
-//route
-app.get("/some", (req, res) => {
-    console.log("sdfsaf");
-    // res.send({success: true,});
-    res.send({message: "all good",});
+const users = [];
+//routes
+app.get("/create/:name", (req, res) => {
+    const user = {
+        name: req.params.name,
+        id: faker.string.uuid(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
+        ipAddress: faker.internet.ip(),
+    };
+
+    users.push(user)
+    console.log(users)
+    res.send({message: `user created ${user.id}, ${user.name} ${user.lastName}, ${user.email}, ${user.ipAddress}`});
+})
+
+app.get("/usersList", (req, res) => {
+    res.send({users});//duomenys siunciami objekte
 });
+
 
 // kaip pasiimti name -> req.params.name
 app.get("/info/:name", (req, res) => {
@@ -15,11 +30,17 @@ app.get("/info/:name", (req, res) => {
     // res.send({success: true,});
     res.send({message: "value of param is " + username,});
 });
+
 app.get("/data", (req, res) => {
     console.log("sfsfd");
     res.send({message: "fghdfh"});
 });
 
+app.get("/some", (req, res) => {
+    console.log("sdfsaf");
+    // res.send({success: true,});
+    res.send({message: "all good",});
+});
 
 app.listen(2002);
 console.log("server run 2002")
