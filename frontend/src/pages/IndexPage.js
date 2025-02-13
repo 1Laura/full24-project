053dc7493ad9f74070f89fca26fcc7e2ab318ee0
Laura <baltrukone@gmail.com>
2025-02-13 08:31:02 +0200

    set posts with zustand

--- frontend/src/pages/IndexPage.js
+++ frontend/src/pages/IndexPage.js
@@ -1,15 +1,17 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import http from "../plugins/https";
 import SinglePost from "../components/SinglePost";
+import useStore from "../store/main";
 
 const IndexPage = () => {
-    const [allPosts, setAllPosts] = useState([]);
+    // const [allPosts, setAllPosts] = useState([]);
+    const {setPosts, posts} = useStore(state => state);
 
     useEffect(() => {
         http("http://localhost:2002/posts")
             .then(data => {
                 console.log(data.posts)
-                setAllPosts(data.posts);
+                setPosts(data.posts);
             })
     }, []);
 
@@ -18,7 +20,7 @@ const IndexPage = () => {
         <div className="container">
             <h3>All posts</h3>
             <div className=" d-flex">
-                {allPosts.map((post, index) =>
+                {posts.map((post, index) =>
                     <SinglePost key={index} userPost={post}/>
                 )}
             </div>
--- frontend/src/store/main.js
+++ frontend/src/store/main.js
@@ -1,8 +1,31 @@
 import {create} from "zustand";
 
 const useStore = create((set) => ({
-    posts: [],
     error: null,
+    posts: [],
+
+    setPosts:(posts)=>set({posts}),
+
+    createPost: (newImageUrl, newTitle) => {
+        set((state => {
+            const newPost = {
+                imageUrl: newImageUrl,
+                title: newTitle,
+                id: Date.now().toString(),
+            };
+            return {
+                posts: [...state.posts, newPost],
+            }
+        }))
+    },
+
+
+    deleteOwnPost: (newPostId) => {
+        set((state) => {
+            return {posts: state.posts.filter(post => post.id !== newPostId)};
+        });
+    },
+
 
     setError: (newError) => set({error: newError}),
 }));
