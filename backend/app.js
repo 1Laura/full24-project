7b2d3124b2b9c5147b471e4660d655628fc18379
Laura <baltrukone@gmail.com>
2025-02-22 20:06:22 +0200

    env and jsonwebtoken started

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+// console.log(process.env.SECRET_KEY)
 const cors = require("cors");
 const {uid} = require("uid");
 const mainRouter = require("./router/routes");
@@ -12,16 +14,6 @@ app.use(express.json())//sitas nustatymas leidzia pasiimi duomenis is req.body(i
 
 app.use("/", mainRouter);//visos uzklausos keliauja i endpointa
 
-
-// app.get("/delete/:id", (req, res) => {
-//     const id = req.params.id;
-//     const postExists = posts.find(post => post.id === id);
-//     if (!postExists) {
-//         return res.send({message: "Post doesn't exist"});
-//     }
-//     posts = posts.filter(post => post.id !== id)
-//     res.send({message: "Post deleted", posts})
-// });
 
 app.listen(2001);
 console.log("server run 2001");
